e8afded6a99dc909f2c04fbd597de4b159169877
Hamza <hamzaabda09@gmail.com>
2024-04-06 10:07:10 +0100

    crud emplois

---
+++ src/app/pages/icons/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:8080/auth';
+
+  constructor(private http: HttpClient) { }
+
+  createOffreEmploi(offreEmploi: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, offreEmploi);
+  }
+
+  deleteOffreEmploi(id: number): Observable<void> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<void>(url);
+  }
+
+  updateOffreEmploi(id: number, offreEmploi: any): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.put<any>(url, offreEmploi);
+  }
+
+  getOffreEmploiById(id: number): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<any>(url);
+  }
+
+  getAllOffresEmploi(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
--- src/app/pages/icons/icons.component.ts
+++ src/app/pages/icons/icons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../icons/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-icons',
@@ -8,8 +10,68 @@ import { Component, OnInit } from '@angular/core';
 export class IconsComponent implements OnInit {
 
   public copy: string;
-  constructor() { }
+  public offresEmploi: any[]; 
+  public nouvelleOffreEmploi: any = {};
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.getAllOffresEmploi();
+  }
+
+  createOffreEmploi(offreEmploi: any) {
+    this.authService.createOffreEmploi(offreEmploi).subscribe(() => {
+      Swal.fire('Succès', 'Offre d\'emploi créée avec succès', 'success').then((result) => {
+        if (result.isConfirmed) {
+          this.reloadPage();
+        }
+      });
+      this.nouvelleOffreEmploi = {};
+    }, error => {
+      Swal.fire('Erreur', 'Erreur lors de la création de l\'offre d\'emploi', 'error');
+      console.error('Erreur lors de la création de l\'offre d\'emploi : ', error);
+      this.nouvelleOffreEmploi = {};
+    });
+  }
+
+  deleteOffreEmploi(id: number) {
+    this.authService.deleteOffreEmploi(id).subscribe(() => {
+      Swal.fire('Succès', 'Offre d\'emploi supprimée avec succès', 'success').then((result) => {
+        if (result.isConfirmed) {
+          this.reloadPage();
+        }
+      });
+      this.getAllOffresEmploi();
+    }, error => {
+      Swal.fire('Erreur', 'Erreur lors de la suppression de l\'offre d\'emploi', 'error');
+      console.error('Erreur lors de la suppression de l\'offre d\'emploi : ', error);
+    });
+  }
+
+  updateOffreEmploi(id: number, offreEmploi: any) {
+    this.authService.updateOffreEmploi(id, offreEmploi).subscribe(() => {
+      Swal.fire('Succès', 'Offre d\'emploi mise à jour avec succès', 'success').then((result) => {
+        if (result.isConfirmed) {
+          this.reloadPage();
+        }
+      });
+      this.getAllOffresEmploi();
+    }, error => {
+      Swal.fire('Erreur', 'Erreur lors de la mise à jour de l\'offre d\'emploi', 'error');
+      console.error('Erreur lors de la mise à jour de l\'offre d\'emploi : ', error);
+    });
+  }
+
+  getAllOffresEmploi() {
+    this.authService.getAllOffresEmploi().subscribe(data => {
+      this.offresEmploi = data;
+    }, error => {
+      Swal.fire('Erreur', 'Erreur lors de la récupération des offres d\'emploi', 'error');
+      console.error('Erreur lors de la récupération des offres d\'emploi : ', error);
+    });
+  }
+
+  reloadPage() {
+    window.location.reload();
   }
 }
